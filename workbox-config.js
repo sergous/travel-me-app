f6e2cc280f8a05b746c669f3ec1ecb3f2a5f9ca1
Sergei Smirnov <sergous@gmail.com>
2025-08-19 15:56:31 -0400

    feat: enhance service worker with caching strategies for fonts and images

--- workbox-config.js
+++ workbox-config.js
@@ -1,6 +1,39 @@
 module.exports = {
-  globDirectory: 'web-build/',
-  globPatterns: ['**/*.{js,ttf,ico,html,json}'],
-  swDest: 'web-build/service-worker.js',
-  ignoreURLParametersMatching: [/^utm_/, /^fbclid$/],
-}
+	globDirectory: "web-dist/",
+	globPatterns: [
+		"**/*.{js,css,html,png,jpg,jpeg,svg,ico,json,woff,woff2,ttf,eot}",
+	],
+	swDest: "dist/service-worker.js",
+	ignoreURLParametersMatching: [/^utm_/, /^fbclid$/],
+	runtimeCaching: [
+		{
+			urlPattern: /^https:\/\/fonts\.googleapis\.com/,
+			handler: "StaleWhileRevalidate",
+			options: {
+				cacheName: "google-fonts-stylesheets",
+			},
+		},
+		{
+			urlPattern: /^https:\/\/fonts\.gstatic\.com/,
+			handler: "CacheFirst",
+			options: {
+				cacheName: "google-fonts-webfonts",
+				expiration: {
+					maxEntries: 30,
+					maxAgeSeconds: 60 * 60 * 24 * 365,
+				},
+			},
+		},
+		{
+			urlPattern: /\.(?:png|jpg|jpeg|svg|gif)$/,
+			handler: "CacheFirst",
+			options: {
+				cacheName: "images",
+				expiration: {
+					maxEntries: 60,
+					maxAgeSeconds: 30 * 24 * 60 * 60,
+				},
+			},
+		},
+	],
+};
